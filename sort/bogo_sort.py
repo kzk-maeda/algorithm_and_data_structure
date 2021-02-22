091cfa112d253082797aa3d112d512a9d92eaf7f
kzk-maeda <kzk.maeda0711@gmail.com>
2021-02-22 23:08:13 +0900

    typing

--- sort/bogo_sort.py
+++ sort/bogo_sort.py
@@ -1,12 +1,22 @@
 import random
+from typing import List
 
+def in_order(numbers: List[int]) -> bool:
+  for i in range(len(numbers)-1):
+    if numbers[i] > numbers[i+1]:
+      return False
+    
+  return True
 
-def bogo_sort(numbers):
+  # all(numbers[i] <= numbers[i+1] for i in range(len(numbers)-1))
+
+def bogo_sort(numbers: List[int]) -> List[int]:
   # print(numbers)
-  random.shuffle(numbers)
+  while not in_order(numbers):
+    random.shuffle(numbers)
   return numbers
 
 if __name__ == '__main__':
-  li = [1,2,3,4,5]
+  li = [1,3,4,2,5]
   print(li)
   print(bogo_sort(li))
