100cf3a13db58c19820956e905aebe2e4f9e539e
kzk-maeda <kzk.maeda0711@gmail.com>
2021-03-30 01:35:08 +0900

    add data structure

---
+++ cpp/data_structure/vector.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  vector<int> a = {4, 3, 12, 7, 11, 1, 9, 8, 14, 6};
+
+  // output list[0]
+  cout << a[0] << endl;
+
+} 
